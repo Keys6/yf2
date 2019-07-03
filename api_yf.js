a6d0f0b07e44aebbd822420826691e8e3d120ce2
Keys6 <46567997+Keys6@users.noreply.github.com>
2019-07-03 21:35:27 +0800

    Update api_yf.js

--- api_yf.js
+++ api_yf.js
@@ -1,1 +1,1 @@
-var newapi="https://www.baidu.com";
+var newapi=["https://www.yfyl.org/","https://www.jinyumaoyi.top/","https://www.yf1881.com/","https://www.yf0369.com/","https://www.yf9669.com/","https://www.yf6996.com/","https://www.yf1956.com/"];
