41bb7ba3594ca331fb7d18b2f01cfa690a1ccb0c
Keys6 <46567997+Keys6@users.noreply.github.com>
2019-07-08 19:00:47 +0800

    Update api_yf.js

--- api_yf.js
+++ api_yf.js
@@ -1,1 +1,1 @@
-var newapi=["https://www.myzke001.com","https://www.efa2.com","https://www.twhw888.com"];
+var newapi=["https://www.efa2.com","https://www.ktz6.com","https://www.twhw888.com","https://www.dongxiyouliao.com","https://www.myzke001.com"];
