076915a00884f061297e123a4c70bb65bdcb22ba
Keys6 <46567997+Keys6@users.noreply.github.com>
2019-07-11 14:53:53 +0800

    Update api_yf.js

--- api_yf.js
+++ api_yf.js
@@ -1,1 +1,1 @@
-var newapi=["https://efa2.com","https://www.twhw888.com","https://www.myzke001.com","https://www.myzke001.com","https://www.dongxiyouliao.com"];
+var newapi=["https://www.twhw888.com","https://efa2.com","https://www.myzke001.com","https://www.myzke001.com","https://www.dongxiyouliao.com"];
